ab1d6bca1af8174ad40c626173222be02129184e
Pamindu Henndige <pamindurh@gmail.com>
2026-05-07 14:06:15 +0530

    implement categories and all coins page

--- components/home/fallback.tsx
+++ components/home/fallback.tsx
@@ -1,6 +1,7 @@
 import DataTable from "@/components/DataTable";
 
 const skeletonRows = Array.from({ length: 6 }, (_, index) => ({ id: index }));
+const categorySkeletonRows = Array.from({ length: 10 }, (_, index) => ({ id: index }));
 
 export const CoinOverviewFallback = () => {
   return (
@@ -65,6 +66,60 @@ export const TrendingCoinsFallback = () => {
         tableClassName="trending-coins-table"
         headerCellClassName="py-3!"
         bodyCellClassName="py-2!"
+      />
+    </div>
+  );
+};
+
+export const CategoriesFallback = () => {
+  const columns: DataTableColumn<(typeof categorySkeletonRows)[number]>[] = [
+    {
+      header: "Category",
+      cellClassName: "category-cell",
+      cell: () => <div className="skeleton category-skeleton" />,
+    },
+    {
+      header: "Top Gainers",
+      cellClassName: "top-gainers-cell",
+      cell: () => (
+        <div className="top-gainers-list">
+          {Array.from({ length: 3 }, (_, index) => (
+            <div key={index} className="skeleton coin-skeleton" />
+          ))}
+        </div>
+      ),
+    },
+    {
+      header: "24h Change",
+      headClassName: "change-header-cell",
+      cellClassName: "change-cell",
+      cell: () => (
+        <>
+          <div className="skeleton value-skeleton-sm" />
+          <div className="skeleton change-icon" />
+        </>
+      ),
+    },
+    {
+      header: "Market Cap",
+      cellClassName: "market-cap-cell",
+      cell: () => <div className="skeleton value-skeleton-lg" />,
+    },
+    {
+      header: "24h Volume",
+      cellClassName: "volume-cell",
+      cell: () => <div className="skeleton value-skeleton-md" />,
+    },
+  ];
+
+  return (
+    <div id="categories-fallback" className="custom-scrollbar">
+      <h4>Top Categories</h4>
+      <DataTable
+        columns={columns}
+        data={categorySkeletonRows}
+        rowKey={(row) => row.id}
+        tableClassName="mt-3"
       />
     </div>
   );
